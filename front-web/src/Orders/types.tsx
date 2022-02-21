fea820bae3b11ea9eb49e1e3c0a073161ca3a680
Everton Santos <dinogame24@gmail.com>
2022-02-20 23:16:12 -0300

    Search order location

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -1,8 +1,16 @@
+import { type } from "os"
+
 export type Product =  {
     id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
+}
+
+export type OrderLocationdata ={
+    latitude: number; 
+    longitude: number;
+    address: string;
 }
 
