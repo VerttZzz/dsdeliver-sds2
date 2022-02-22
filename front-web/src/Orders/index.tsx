19a3d2282592819b7b79f873dc123a9a2562fb4c
Everton Santos <dinogame24@gmail.com>
2022-02-22 01:31:01 -0300

    Finish front-web

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react';
-import './styles.css';
+import { toast } from 'react-toastify';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
-import { OrderLocationdata, Product } from './types';
-import { fetchProducts } from '../api';
+import { OrderLocationData, Product } from './types';
+import { fetchProducts, saveOrder } from '../api';
 import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
 import Footer from '../Footer';
 import { chekIsSelected } from './helpers';
+import './styles.css';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([])
     const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
-    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+      return sum + item.price;
+    }, 0);
     
     useEffect(() => {
         fetchProducts()
         .then( response => setProducts(response.data))
-        .catch(error =>console.log(error))
+        .catch(() =>
+          toast.warning('Erro ao listar pedidos');
     }, []);
 
     const handleSelectProduct = (product: Product) => {
@@ -30,6 +35,23 @@ function Orders() {
           setSelectedProducts(previous => [...previous, product]);
         }
       }
+
+      const handleSubmit = () => {
+        const productsIds = selectedProducts.map(({ id }) => ({ id }));
+        const payload = {
+          ...orderLocation!,
+          products: productsIds
+        }
+      
+        saveOrder(payload)
+        .then((response) => {
+          toast.error(`Pedido enviado com sucesso! Nº ${response.data.id}`);
+          setSelectedProducts([]);
+        })
+          .catch(() => {
+            toast.warning('Erro ao enviar pedido');
+          })
+      }
       
     return (
      <>
@@ -41,7 +63,11 @@ function Orders() {
              selectedProducts={selectedProducts}
              />
             <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-            <OrderSummary />
+            <OrderSummary 
+            amount={selectedProducts.length}
+            totalPrice={totalPrice}
+            onSubmit={handleSubmit}
+             />
         </div>
         <Footer />
      </>
